1942fb148a44a7a0d604abdc55b890811c4d0124
Iván <ivan_oia91@hotmail.com>
2025-06-14 19:06:16 +0200

    Actualizar imagenesCartas.js

--- src/assets/imagenesCartas.js
+++ src/assets/imagenesCartas.js
@@ -1,5 +1,5 @@
 export function imagenesPokemon() {
-  const ids = ['1', '2', '4', '6', '7', '9', '25', '26', '133']
+  const ids = ['1', '3', '4', '6', '7', '9', '25', '26', '150']
   const pokemons = ids.map((id) => ({
     id,
     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
